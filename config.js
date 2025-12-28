aae31685cb6ea54495a1814dccbaa4171c6576c2
Renvian <vianrenvil@gmail.com>
2025-12-28 17:51:42 +0530

    Restore API keys in config.js
    Restored API keys for Supabase and Gemini in config.js.

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = 'https://qnthrypjrygcaikgbjrp.supabase.co'; 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFudGhyeXBqcnlnY2Fpa2dianJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4OTI0ODYsImV4cCI6MjA4MTQ2ODQ4Nn0.v3IGSBPpRYMGCTkd3NOOMvi9zJTKpsJIYT7ZH8hQPoQ';
-const GEMINI_API_KEY = 'ENTER YOUR API KEY';
+const GEMINI_API_KEY = 'ENTER YOUR API KEY'; //USES gemini-2.0-flash
+
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
